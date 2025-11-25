d3b606e5b78060f71434749bb4915e2b6f8bb472
ronicleal <ralealp01@iesalbarregas.es>
2025-11-25 23:34:21 +0100

    🔥Arreglo de la funcion cargarProvincias

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-// VARIABLES GLOBALES
-let todasLasGasolineras = [];
-let gasolinerasMunicipio = [];
+//1-Guardo en variables las url de provincia
+const urlProvincias = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Provincias/"
 
-//1) FUNCION PARA CARGAR TODAS LAS GASOLINERAS UNA SOLA VEZ
-async function cargasTodas() {
-    const url = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/";
-    const respuesta = await fetch(url);
-    //Trasnformar la respuesta en un objeto JS
+//2-Funcion para cargar las provincias
+async function cargarProvincias() {
+    const respuesta = await fetch(urlProvincias);
     const datos = await respuesta.json();
-    //Guardo todas las gasolineras en una variable global
-    todasLasGasolineras = datos.ListaEESSPrecio;
+    const select = document.getElementById("provincia");
 
-    console.log(todasLasGasolineras)
-    cargarProvincias()
+    datos.forEach(provincia => {
+        const op = document.createElement("option");
+        op.value = provincia.IDPovincia;
+        op.textContent = provincia.Provincia;
+        select.appendChild(op);
+    })
    
 }
 
-//2) FUNCION PARA CARGAR PROVINCIAS
-function cargarProvincias(){
-    const select = document.getElementById("provincia");
-
-    const provincias = [...new Set(todasLasGasolineras.map(g => g.Provincia))].sort();
-
-    provincias.forEach(p =>{
-        const op = document.createElement("option");
-        op.value = p;
-        op.textContent = p;
-        select.appendChild(op);
-
-    });
-}
-
-
-
-
-// CARGAR TODO AL INICIO
-cargasTodas()
+cargarProvincias()

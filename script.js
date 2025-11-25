f11940689974b4b4e1cb8db4fe9adb3d83b0ae86
ronicleal <ralealp01@iesalbarregas.es>
2025-11-25 23:48:28 +0100

    🔥Arreglo campos de provincia y municipio index.html

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //1-Guardo en variables las url de provincia
 const urlProvincias = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Provincias/"
+const urlMunicipios = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/MunicipiosPorProvincia/"
+
+
+
+
 
 //2-Funcion para cargar las provincias
 async function cargarProvincias() {
@@ -16,4 +21,17 @@ async function cargarProvincias() {
    
 }
 
+//3-Funcion para cargar los municipios
+async function cargarMunicipios(idProvincia){
+    
+
+}
+
+
+
+//Cargar al inicio
 cargarProvincias()
+
+
+//Eventos
+document.getElementById("provincia").addEventListener("change")
